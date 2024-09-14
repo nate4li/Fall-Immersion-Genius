4afbac4cac532cd20c72e25f6cd078c4cf108e49
admin <admin@oak12blk-Mac21.local>
2024-09-14 11:51:17 -0700

    finished counter app

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,6 @@
 function counter() {
 // Write your code below. Hint: you can finish it in about 3-5 lines.
+alert ("I am Tre")
+p_tag_content = document.getElementryById('counter-number');
+p_tag_content.innerHTML = Number(p_tag_content.innerHTML) + 1;
 }
