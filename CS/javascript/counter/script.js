a5a0512269f0c66d49bdeed049c1455c3d228fc4
admin <admin@oak12blk-Mac4.local>
2024-09-14 11:51:11 -0700

    finished counter app

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,7 @@
 function counter() {
 // Write your code below. Hint: you can finish it in about 3-5 lines.
+p_tag_content = document.getElementById("counter-number");
+p_tag_content.innerHTML = Number(p_tag_content.innerHTML) + 1;
+
+
 }
