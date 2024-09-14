2686378410b226e3f85fa7e864df411b0c1d6389
admin <admin@OakBlk1-Locked.local>
2024-09-14 11:51:28 -0700

    finshed counter app

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,5 @@
 function counter() {
 // Write your code below. Hint: you can finish it in about 3-5 lines.
+P_tag_content = document.getElementById('counter-number');
+P_tag_content.innerHTML = Number(P_tag_content.innerHTML) + 1;
 }
