5f0b427f54991dfec51588ccca24b036816524ab
neoshtml <xavier.lunarmstead@hgs.hiddengeniusproject.org>
2024-09-25 18:13:55 -0700

    added event handling

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,4 @@
 function darkMode() {
-// Your code here.
+    document.body.style.backgroundColor = "black";
+    document.getElementsByTagName('h3') [0].style.color = "white";
 }
--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,6 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked');
+    image_tag = document.createElement('img');
+    image_tag = 'https://images-prod.dazeddigital.com/560/azure/dazed-prod/1060/8/1068776.jpg';
+    document.body.appendChild(image_tag);
+})
