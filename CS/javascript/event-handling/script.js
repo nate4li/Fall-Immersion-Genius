028f30b700f2446fb721943977e950b60d85176b
admin <admin@oak12blk-Mac4.local>
2024-09-25 18:19:43 -0700

    added event handling

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,8 @@
-function eventHandling() {
-    // Your code here.
-}
+
+
+document.getElementById("myButton").addEventListener('click', function() {
+    alert("Button clicked");
+    image_tag = document.createElement("img");
+    image_tag.src = "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Sunflower_from_Silesia2.jpg/1200px-Sunflower_from_Silesia2.jpg";
+    document.body.appendChild(image_tag);
+});
