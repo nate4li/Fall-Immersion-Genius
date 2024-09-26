56afa624a376302902684eb5f6c278ff74b4a66c
admin <admin@The-Hidden-Genius-Project.local>
2024-09-25 18:14:24 -0700

    added event handling

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,6 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('button clicked');
+    image_tag = document.createElement('img');
+    image_tag.src = 'https://media.grassroots365.com/player-photo-upload/admin/2023-04-07-11-45-EBC-Bay-Area-2023-683.jpg';
+    document.body.apprendChild(image_tag);
+});
