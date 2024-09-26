b2e50e3529f9021f684d6a7e6a182c27b9601fa2
admin <admin@Adagio-Hidden-Genius-Project-3.local>
2024-09-25 18:23:02 -0700

    added event handling

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,9 @@
 function eventHandling() {
     // Your code here.
 }
+document.getElementById('myButton').addEventListener('click',function() {
+    alert('Button clicked')
+    image_tag = document.createElement('img');
+    image_tag.src = 'https://i.kym-cdn.com/entries/icons/original/000/050/103/the_downfall_of_diddy.jpg';
+    document.body.appendChild(image_tag);
+})
