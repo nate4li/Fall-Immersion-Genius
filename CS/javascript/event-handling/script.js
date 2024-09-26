dea52b6c484faeb99c350212678b8160291e8f8d
admin <admin@The-Hidden-Genius-Project-4.local>
2024-09-25 18:14:49 -0700

    added event handling

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,6 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function() {
+alert('Button clicked');
+image_tag = document.createElement ( 'img');
+image_tag.src = 'https://cdn.prod.website-files.com/5d7e8885cad5174a2fcb98d7/64933f98a477f02e36a282d1_5eddd950e5cf1ec1fa5c2d83_virtual-influencer-john-pork.jpeg';
+document.body.appendChild(image_tag);
+})
