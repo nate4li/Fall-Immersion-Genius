a43a50e0ba42fc58715f5bef2bb622963e8807d8
admin <admin@The-Hidden-Genius-Project.local>
2024-09-25 18:23:53 -0700

    added event handling

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,6 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function(){
+    alert('Button clicked')
+    image_tag = document.createElement('img');
+    image_tag.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Sunset_2007-1.jpg/1200px-Sunset_2007-1.jpg';
+    document.body.appendChild(image_tag);
+});
