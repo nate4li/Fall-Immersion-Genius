d159aa43d6384f8f4fb57c9c75772964cecf143f
admin <admin@oak12blk-Mac21.local>
2024-09-25 18:21:57 -0700

    added event-handler

--- CS/javascript/dark-mode/script.js
+++ CS/javascript/dark-mode/script.js
@@ -1,3 +1,4 @@
 function darkMode() {
-// Your code here.
+    document.body.style.background = "black";
+    document.getElementsByTagName('h3').style.color = "white";
 }
--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,7 @@
-function eventHandling() {
-    // Your code here.
-}
+
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked')
+    image_tag = document.createElement('img');
+    image_tag.src='https://cdn.vectorstock.com/i/500p/57/62/message-box-questions-for-any-purposes-mark-vector-39825762.jpg';
+    document.body.appendChild(image_tag);
+});
