d0b2ee66ec648bd73828a884c904b41e5c490ddd
admin <admin@Alexs-Mac.local>
2024-09-25 18:15:12 -0700

    added event handling

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,6 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked');
+    image_tag = document.createElement('img');
+    image_tag.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwkjmzOBvkKgwBzV_nBKKZPjWyqu_4-NUIvg&s';
+    document.body.appendChild(image_tag);
+});
