1999954dee5fa3460065f813fb4f4b1711b1fffe
admin <adminadmin@Mysterious-OakBlk-device-to-be-locked1.local>
2024-09-25 18:26:07 -0700

    added event handling

--- CS/javascript/dark-mode/script.js
+++
@@ -1,3 +0,0 @@
-function darkMode() {
-// Your code here.
-}
--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,6 @@
-function eventHandling() {
-    // Your code here.
-}
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked');
+    image_tag = document.createElement('img');
+    image_tag.src = "https://m.media-amazon.com/images/S/pv-target-images/9e103a32ea9441929166954798ca14f8a6e913c040e09e9065d207b1bd31e116.jpg";
+    document.body.appendChild(image_tag);
+});
