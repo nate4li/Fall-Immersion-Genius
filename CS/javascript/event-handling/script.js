684d386dac2e7abd3329fbeb9fc11324e90e9f0a
Abe <admin@Abraham-Keletas-Macbook-Pro.local>
2024-10-02 18:32:10 -0700

    finished form submission

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,8 @@
-function eventHandling() {
-    // Your code here.
-}
+// Event handling in JavaScript
+document.getElementById('myButton').addEventListener('click', function() {
+    alert('Button clicked');
+    image_tag = document.createElement('img');
+    image_tag.src = 'https://naruto-official.com/common/ogp/NTOS_OG-main.png';
+    document.body.appendChild(image_tag);
+});
+ 
