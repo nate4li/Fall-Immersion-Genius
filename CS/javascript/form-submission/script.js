5ff54963e8798191f494b954ad526f392cab4412
admin <adminadmin@Mysterious-OakBlk-device-to-be-locked1.local>
2024-10-02 18:27:45 -0700

    Finished Form Submsiion

--- CS/javascript/form-submission/script.js
+++ CS/javascript/form-submission/script.js
@@ -1,3 +1,5 @@
-function formSubmission() {
-    // Your code here.
-}
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Form Submitted');
+    location.reload();
+});
