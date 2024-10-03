af1d64488d67b5efeae32c927398c772596145ae
Admin <admin@oak12gld-Mac16.local>
2024-10-02 18:28:54 -0700

    finished form submission

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -3,5 +3,5 @@ document.getElementById('myButton').addEventListener('click', function(){
     alert('Button clicked');
     image_tag = document.createElement('img');
     image_tag.src = 'https://pbs.twimg.com/profile_images/1642336301948055552/ooPCfJEl_400x400.jpg';
-    document.body.appendChild(image_tag)
+    document.body.appendChild(image_tag);
 });   
