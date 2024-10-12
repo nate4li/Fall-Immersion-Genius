cf1ed50e0d12c278d532e87f52a7944a5a854f00
nate4li <nathanielmjr10@gmail.com>
2024-10-12 11:11:31 -0400

    Update script.js

--- CS/javascript/counter/script.js
+++ CS/javascript/counter/script.js
@@ -1,3 +1,5 @@
 function counter() {
 // Write your code below. Hint: you can finish it in about 3-5 lines.
+  counter_num = document.getElementById('counter-number');
+  counter_num.innerHTML = parseInt(counter_num.innerHTML) + 1;
 }
