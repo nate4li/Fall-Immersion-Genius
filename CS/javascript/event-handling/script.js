cefbe3a9a784b57a758f8ded61770767b5770dfe
nate4li <nathanielmjr10@gmail.com>
2024-11-17 10:23:59 -0500

    Update script.js

--- CS/javascript/event-handling/script.js
+++ CS/javascript/event-handling/script.js
@@ -1,3 +1,14 @@
 function eventHandling() {
-    // Your code here.
+    // <!DOCTYPE html> 
+<html land="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style.css">
+ </head> 
+ <body>
+   <button id="MyButton">click</button>
+   <script src="script.js"></script>
+ </body>
+</html>
 }
